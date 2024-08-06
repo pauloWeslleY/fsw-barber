33316f87d2ff68fc6bdc390c307e28644949b5c4
pauloWeslleY <weslleyspfc@13outlook.com>
2024-08-05 23:07:13 -0300

    chore: add schadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { Button } from "./components/ui/button"
+
 const Home = () => {
-  return <h1 className="mb-5 bg-red-200 text-red-500">home page</h1>
+  return <Button>teste</Button>
 }
 
 export default Home
